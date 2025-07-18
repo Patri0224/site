a3a082a57bde5c606db1417dbb4fec58a2dd6eba
Patrizio <106958889+Patri0224@users.noreply.github.com>
2025-07-18 23:07:43 +0200

    Update script.js

--- personal/punteggio1/script.js
+++ personal/punteggio1/script.js
@@ -1,5 +1,5 @@
 //variabili globali
-const persone = { 0: "", 1: "Alice", 2: "Andre", 3: "Busti", 4: "Dani", 5: "Fede", 6: "Fra", 7: "Friggi", 8: "Giorgia", 9: "Giulia", 10: "Marco", 11: "Mati", 12: "Pat", 13: "Totta", 14: "Viola", 15: "Margo", 16:"Lisa", 17:"Mirko", 18:"Depa", 19:"Giada", 20:"Irene",21:"Mati D",22:"Clara", 23:"Samu", 24:"Irene2" , 25:"Giuse", 26:"Paolo"}
+const persone = { 0: "", 1: "Alice", 2: "Andre", 3: "Busti", 4: "Dani", 5: "Fede", 6: "Fra", 7: "Friggi", 8: "Giorgia", 9: "Giulia", 10: "Marco", 11: "Mati", 12: "Pat", 13: "Totta", 14: "Viola", 15: "Margo", 16:"Lisa", 17:"Mirko", 18:"Depa", 19:"Giada", 20:"Irene",21:"Mati D",22:"Clara", 23:"Samu", 24:"Billa" , 25:"Giuse", 26:"Paolo"}
 //Ludovica, Giorgia c, selina
 let numPersone = 27;
 var premuto = false;//per conferma indietro
